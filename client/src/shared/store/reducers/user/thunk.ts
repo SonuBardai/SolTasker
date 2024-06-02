3791e3a6fe7d22c5ebe78d694f7554fae6a683f8
SonuBardai <sonubardai9@gmail.com>
2024-06-02 21:29:03 +0530

    fix typing in reducer

--- client/src/shared/store/reducers/user/thunk.ts
+++ client/src/shared/store/reducers/user/thunk.ts
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { PublicKey } from "@solana/web3.js";
 import getClient from "../../../api/config";
+import { AxiosResponse } from "axios";
 
 export const login = createAsyncThunk("user/login", async (publicKey: PublicKey) => {
   const apiClient = getClient();
-  const response = await apiClient.webAuthRouterLogin({ LoginPayload: { public_key: publicKey.toBase58() } });
+  const response = (await apiClient.webAuthRouterLogin({ LoginPayload: { public_key: publicKey.toBase58() } })) as AxiosResponse;
   return response.data;
 });
