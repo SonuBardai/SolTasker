b86a9e720d7d18b2d12292603a9a884e2da9bb4e
SonuBardai <sonubardai9@gmail.com>
2024-06-03 09:59:33 +0530

    use mode env var

--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_NAME = "SolTasker";
 export const EMAIL = "";
 export const RENDER_BACKEND_URL = "https://soltasker.onrender.com";
-export const BACKEND_URL = import.meta.env.BACKEND_URL || (import.meta.env.MODE === "production" && RENDER_BACKEND_URL) || "http://localhost:8000";
+export const BACKEND_URL = import.meta.env.BACKEND_URL || (import.meta.env.VITE_VERCEL_ENV === "production" && RENDER_BACKEND_URL) || "http://localhost:8000";
 export const COMMISSION_PERCENTAGE = 0.02;
