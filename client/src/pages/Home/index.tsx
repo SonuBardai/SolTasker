fed09d9f44af64143fb3dc0d851971d141dac903
SonuBardai <sonubardai9@gmail.com>
2024-06-03 09:19:49 +0530

    debug change

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -11,12 +11,8 @@ const Home = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Woah! Welcome to {PROJECT_NAME}!</h1>
-          <div>
-            Backend url: {import.meta.env.BACKEND_URL} {process.env.BACKEND_URL}
-          </div>
-          <div>
-            Node env: {import.meta.env.NODE_ENV} {process.env.NODE_ENV}
-          </div>
+          <div>Backend url: {import.meta.env.BACKEND_URL}</div>
+          <div>Node env: {import.meta.env.NODE_ENV}</div>
           <p className="py-6">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam voluptatum expedita quisquam autem facere dolores deleniti modi fugiat at, sed odio quam odit aliquid
             iusto exercitationem distinctio labore a perferendis, suscipit, assumenda possimus reprehenderit. Beatae cumque deleniti dolorum qui deserunt!
