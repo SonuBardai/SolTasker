7d1fd50c1f7eecf9490e618dc1d7141b325d03c2
SonuBardai <sonubardai9@gmail.com>
2024-06-03 06:36:58 +0530

    fix status router

--- server/sol_tasker/urls.py
+++ server/sol_tasker/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 from web_auth.router import router as web_auth_router
+from .routes.status import router as status_router
 
 
 api = NinjaAPI()
+api.add_router("", status_router)
 api.add_router("", web_auth_router)
 
 urlpatterns = [
