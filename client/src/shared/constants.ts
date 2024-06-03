118dc58b6b25eafeb45c63180ac0f02a08d96174
SonuBardai <sonubardai9@gmail.com>
2024-06-03 09:42:07 +0530

    add logic to use backend url by vite env

--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -1,4 +1,5 @@
 export const PROJECT_NAME = "SolTasker";
 export const EMAIL = "";
-export const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:8000";
+export const RENDER_BACKEND_URL = "https://soltasker.onrender.com";
+export const BACKEND_URL = import.meta.env.BACKEND_URL || (import.meta.env.VITE_VERCEL_ENV === "production" && RENDER_BACKEND_URL) || "http://localhost:8000";
 export const COMMISSION_PERCENTAGE = 0.02;
