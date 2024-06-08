8d28b6386b7867691983f488d47b085cead66b8c
SonuBardai <sonubardai9@gmail.com>
2024-06-08 16:58:36 +0530

    fix router

--- server/sol_tasker/urls.py
+++ server/sol_tasker/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 from web_auth.router import router as web_auth_router
-from .routes.status import router as status_router
+from task.router import router as task_router
 
 
 api = NinjaAPI()
-api.add_router("", status_router)
 api.add_router("", web_auth_router)
+api.add_router("", task_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- server/task/router.py
+++ server/task/router.py
@@ -1,18 +1,15 @@
-# from ninja import Router
-# from pydantic import BaseModel
+from ninja import Router
+from pydantic import BaseModel
 
 
-# router = Router()
+router = Router()
 
 
-# class CreateTaskPayload(BaseModel):
-#     ...
+class CreateTaskPayload(BaseModel): ...
 
 
-# class CreateTaskResponse(BaseModel):
-#     ...
+class CreateTaskResponse(BaseModel): ...
 
 
-# @router.post("task")
-# def create_task(request, payload: CreateTaskPayload) -> CreateTaskResponse:
-#     ...
+@router.post("task")
+def create_task(request, payload: CreateTaskPayload) -> CreateTaskResponse: ...
