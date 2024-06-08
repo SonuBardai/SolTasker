3bd9b06bb66687edb23382e65486d59a43424940
SonuBardai <sonubardai9@gmail.com>
2024-06-08 12:38:28 +0530

    fix shell script

--- server/build.sh
+++ server/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Install Python
-apt-get update
-apt-get install python3 python3-pip
-
-# Install pipenv
-pip3 install --user pipenv
+pip install --user pipenv
 
 # Run pipenv install
 pipenv install
