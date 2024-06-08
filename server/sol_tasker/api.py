5acbe1b8c145450f46cdf8bf2244c997c03da5db
SonuBardai <sonubardai9@gmail.com>
2024-06-08 17:24:34 +0530

    add healthcheck route

--- server/sol_tasker/api.py
+++ server/sol_tasker/api.py
@@ -1,8 +1,12 @@
 from ninja import NinjaAPI
 from task.api import router as task_router
 from web_auth.api import router as web_auth_router
+from .healthcheck.api import router as healthcheck_router
 
 
 api = NinjaAPI()
+
 api.add_router("", web_auth_router)
 api.add_router("", task_router)
+
+api.add_router("", healthcheck_router)
